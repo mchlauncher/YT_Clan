c4d4c8c35c4f295d51e2e2f70cb285e27f30405b
go-tiger <dev.gotiger@gmail.com>
2025-02-14 14:55:36 +0900

    v1.0.1

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://github.com/CustomLauncherLab/Launcher-distro-file/raw/main/YT_Clan/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://github.com/mchdistro/YT_Clan/raw/main/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
